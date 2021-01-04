b02386627e922763e6cbefb8622bc2aa770bda20
Luis <Luis@LAPTOP-PTJO4A3R>
2021-01-04 12:32:16 -0600

    servicio y termino en forms template

--- src/app/pages/tamplate/tamplate.component.ts
+++ src/app/pages/tamplate/tamplate.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { PaisService } from '../../services/pais.service';
 
 @Component({
   selector: 'app-tamplate',
@@ -7,9 +9,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TamplateComponent implements OnInit {
 
-  constructor() { }
+  usuario= {
+    nombre: '',
+    apellido: '',
+    correo: '',
+    pais: '',
+    genero: 'M'
+  }
+  paises: any[] = [];
+
+  constructor(private PaisService : PaisService) { }
 
   ngOnInit(): void {
+    this.PaisService.getPaises().subscribe( paises => {
+      this.paises = paises;
+
+
+      //VALOR DE INICIO DEL SELECT
+      this.paises.unshift({
+        nombre: '[Seleccione Pais]',
+        codigo: ''
+      })
+    });
   }
 
+  guardar(formulario: NgForm){
+
+    if( formulario.invalid){
+
+      Object.values( formulario.controls).forEach( control => { control.markAllAsTouched() });
+
+      return;
+    }
+    console.log(formulario.value);
+  }
 }
